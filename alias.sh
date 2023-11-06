6363f94725c7ac779ae7a6ff9f92154b8ccf900d
Yin-Chi Chan <ycc39@cam.ac.uk>
2023-11-06 11:11:03 +0000

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
 alias dprune='docker system prune --volumes'
-alias dbu='docker compose down && docker compose build && docker compose up'
+alias dbu='docker compose down -t 3 && docker compose up --build -d --remove-orphans'
